1c8ce14b6f2c523aa7874a4d408fe6800f7bbee3
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-04-06 00:38:49 +0530

    Add file read functionality

--- 3-File Handling/file-watcher/index.js
+++ 3-File Handling/file-watcher/index.js
@@ -1,11 +1,28 @@
 import * as fs from "node:fs/promises";
+import { Buffer } from "node:buffer";
 
-async function watch(file) {
-  const watcher = fs.watch(file);
+async function watch(path) {
+  const fileWrapper = await fs.open(path);
+
+  const watcher = fs.watch(path);
+
   for await (const event of watcher) {
     if (event.eventType === "change") {
+      // The file was changed
       console.log("The file was changed.");
+
+      // Set up config variables.
+      const { size } = await fileWrapper.stat();
+      const buffer = Buffer.alloc(size);
+      const offset = 0;
+      const pos = 0;
+      const length = size;
+
+      // Read file
+      const fileContent = await fileWrapper.read(buffer, offset, length, pos);
+      console.log(fileContent);
     }
   }
 }
+
 watch("./command.txt");
