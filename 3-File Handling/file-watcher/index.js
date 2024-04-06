3aed1e1d26b72a0d89cfa7b2ba2bf2b9ef6758cc
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-04-06 06:59:05 +0530

    Add event listeners to all create, read, add, delete functionality

--- 3-File Handling/file-watcher/index.js
+++ 3-File Handling/file-watcher/index.js
@@ -2,6 +2,8 @@ import * as fs from "node:fs/promises";
 import { EventEmitter } from "node:events";
 import { Buffer } from "node:buffer";
 
+import { getCommandData } from "./utils/get-command-data.js";
+
 const emitter = new EventEmitter();
 
 emitter.on("change", async (fileWrapper) => {
@@ -22,7 +24,36 @@ emitter.on("change", async (fileWrapper) => {
 
   // Decode the buffer
   const fileContent = fileBuffer.toString("utf-8");
-  console.log(fileContent);
+  const { method, file, payload } = getCommandData(fileContent);
+
+  if (
+    method !== "create" &&
+    method !== "add" &&
+    method !== "rename" &&
+    method !== "delete"
+  )
+    emitter.emit("error", method);
+  else emitter.emit(method, file, payload);
+});
+
+emitter.on("create", async (file, payload) => {
+  console.log("Created a file");
+});
+
+emitter.on("delete", async (file, payload) => {
+  console.log("Deleted a file");
+});
+
+emitter.on("add", async (file, payload) => {
+  console.log("Appended to a file");
+});
+
+emitter.on("rename", async (file, payload) => {
+  console.log("Renamed a file");
+});
+
+emitter.on("error", async () => {
+  console.log("Caught an error");
 });
 
 async function watch(path) {
