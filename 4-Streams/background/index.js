ee0ed5b4d6e6065a95f10b330b77085ec0f26239
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-04-27 19:27:47 +0530

    Add efficient stream solution

--- 4-Streams/background/index.js
+++ 4-Streams/background/index.js
@@ -1,5 +1,6 @@
 import * as fsPromise from "node:fs/promises";
 import * as fsCallback from "node:fs";
+import { Buffer } from "node:buffer";
 
 // This code took 1m7s to run, 40ish MB in the memory and 34% of the CPU
 async function writeUsingPromise(filepath) {
@@ -27,35 +28,68 @@ function writeUsingCallback(filepath) {
       return;
     }
     for (let i = 1; i <= 1000_000; i++) {
-      fsCallback.writeSync(fd,i + "\n");
+      fsCallback.writeSync(fd, i + "\n");
     }
-    console.timeEnd("duration")
+    console.timeEnd("duration");
   });
 }
 
-
-// This code took 1.1ish to run, 250ish MB in the memory and 22% of the 
+// This code took 1.1ish to run, 250ish MB in the memory and 22% of the CPU
 // This is a naive implementation which is not so memory efficient.
-async function writeUsingStreams(filepath){
+async function writeUsingStreams(filepath) {
   let fileHandler;
-  console.time("duration")
-  try{
-    fileHandler = await fsPromise.open(filepath,"w");
-    const writeStream = fileHandler.createWriteStream()
-    for(let i = 1; i<=1000_000; i++){
-        await writeStream.write(i+"\n")
+  console.time("duration");
+  try {
+    fileHandler = await fsPromise.open(filepath, "w");
+    const writeStream = fileHandler.createWriteStream();
+    for (let i = 1; i <= 1000_000; i++) {
+      writeStream.write(i + "\n");
     }
-  }catch(e){
-    console.log("Unable to write to file.")
-  }finally{
-    if(fileHandler)
-      fileHandler.close()
+  } catch (e) {
+    console.log("Unable to write to file.");
+  } finally {
+    if (fileHandler) fileHandler.close();
 
-    console.timeEnd("duration")
+    console.timeEnd("duration");
   }
+}
+
+// This code took 6.1ish to run, 26ish MB in the memory and 26% of the CPU
+async function writeUsingStreamsEfficient(filepath) {
+  let fileHandler;
+  console.time("duration");
+  let start = 1;
+  try {
+    fileHandler = await fsPromise.open(filepath, "w");
+    const writeStream = fileHandler.createWriteStream();
+
+    function writeToStream(toWriteFrom) {
+      for (let i = toWriteFrom; i <= 1000_000; i++) {
+        const myBuffer = Buffer.from(i + "\n");
+        const canWrite = writeStream.write(myBuffer);
+        start = i + 1;
+        if(i === 1000_000) writeStream.end()
+        if (!canWrite) break;
+      }
+    }
+
+    writeToStream(start);
+
+    writeStream.on("drain", () => {
+      writeToStream(start);
+    });
+
+    writeStream.on("finish",async ()=>{
+      await fileHandler.close()
+      console.timeEnd("duration")
+    })
+
+  } catch (e) {
+    console.log("Unable to write to file.");
+  } 
 }
 
 // writeUsingPromise("./test.txt");
 // writeUsingCallback("./test.txt");
 // writeUsingStreams("./test.txt")
-
+writeUsingStreamsEfficient("./test.txt");
